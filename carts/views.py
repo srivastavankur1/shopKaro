10c3057f2915850ce20ab1db3fa17200bcabed0e
Ankur Srivastava <srivastavaankur987@gmail.com>
2026-02-23 12:26:49 +0530

    important fixes

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from store.models import Product, Variation
 from carts.models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -11,7 +12,6 @@ def _cart_id(request):
     if not cart:
         cart = request.session.create()
     return cart
-
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)   #get the product
@@ -97,7 +97,6 @@ def remove_cart_item(request, product_id, cart_item_id):
     cart_item.delete()
     return redirect('cart')
 
-
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax=0
@@ -121,3 +120,28 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'grand_total':grand_total,
     }
     return render(request, 'store/cart.html', context)
+
+@login_required(login_url='login')
+def checkout(request,total=0, quantity=0, cart_items=None):
+    try:
+        tax=0
+        grand_total=0
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        
+        tax = (2 * total)/100
+        grand_total = total + tax
+
+    except ObjectDoesNotExist:
+        pass
+    context = {
+        'total': total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+        'tax': tax,
+        'grand_total':grand_total,
+    }
+    return render(request, 'store/checkout.html', context)
--- shopkaro/settings.py
+++ shopkaro/settings.py
@@ -143,5 +143,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER = 'srivastavaankur987@gmail.com'
-EMAIL_HOST_PASSWORD = 'rbrq stni nblu uyia'
+EMAIL_HOST_PASSWORD = 'your gmail app password'
 EMAIL_USE_TLS = True
