c6adab73e053559071247ab751e6eacbe7f9cf49
Ankur Srivastava <srivastavaankur987@gmail.com>
2026-02-23 11:35:45 +0530

    login functionality done

--- shopkaro/settings.py
+++ shopkaro/settings.py
@@ -132,3 +132,16 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+
+#smtp configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT= 587
+EMAIL_HOST_USER = 'srivastavaankur987@gmail.com'
+EMAIL_HOST_PASSWORD = 'rbrq stni nblu uyia'
+EMAIL_USE_TLS = True
